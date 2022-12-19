1fc27734026cac4d75afde3ae07d1d3bd86a44d0
Bart Lyons <lyons.bart@protonmail.com>
2022-12-19 03:55:49 -0500

    Search working.

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -1,4 +1,5 @@
 ﻿using Blog_MVC.Data;
+using Blog_MVC.Enums;
 using Blog_MVC.Models;
 using Blog_MVC.Services;
 using Microsoft.AspNetCore.Http;
@@ -28,6 +29,40 @@ namespace Blog_MVC.Controllers
             _imageService = imageService;
             _userManager = userManager;
         }
+
+        public async Task<IActionResult> SearchIndex(int? page, string searchTerm)
+        {
+            ViewData["SearchTerm"] = searchTerm;
+
+            var pageNumber = page ?? 1;
+            var pageSize = 5;
+
+
+            var posts = _context.Posts.Where(p => p.ReadyStatus == ReadyStatus.ProductionReady).AsQueryable();
+            if (searchTerm != null)
+            {
+                searchTerm = searchTerm.ToLower();
+
+                posts = posts.Where(
+                    p => p.Title.ToLower().Contains(searchTerm) ||
+                         p.Abstract.ToLower().Contains(searchTerm) ||
+                         p.Content.ToLower().Contains(searchTerm) ||
+                         p.Comments.Any(c => c.Body.ToLower().Contains(searchTerm) ||
+                                             c.ModeratedBody.ToLower().Contains(searchTerm) ||
+                                             c.BlogUser.FirstName.ToLower().Contains(searchTerm) ||
+                                             c.BlogUser.LastName.ToLower().Contains(searchTerm) ||
+                                             c.BlogUser.Email.ToLower().Contains(searchTerm)));
+            }
+
+            posts = posts.OrderByDescending(p => p.Created);
+
+            return View(await posts.ToPagedListAsync(pageNumber, pageSize));
+
+        }
+
+
+
+
 
         // GET: Posts
         public async Task<IActionResult> Index()
